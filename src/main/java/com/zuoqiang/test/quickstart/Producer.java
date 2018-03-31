d08075270c904ef0bbfe87db538c3cdc04585609
zuoqiang <756465185@qq.com>
2018-03-31 18:38:29 +0800

    Producer 设置重试

--- src/main/java/com/zuoqiang/test/quickstart/Producer.java
+++ src/main/java/com/zuoqiang/test/quickstart/Producer.java
@@ -9,13 +9,15 @@ public class Producer {
     public static void main(String[] args) throws InterruptedException, MQClientException {
         DefaultMQProducer producer = new DefaultMQProducer("quick_producer");
         producer.setNamesrvAddr("10.211.55.9:9876;10.211.55.13:9876");
+        //与下面的发送超时设置配合使用，设置重试次数。
+        producer.setRetryTimesWhenSendFailed(3);
         producer.start();
 
         for (int i = 0; i < 100; i++) {
             Message message = new Message("TopicQuickStart", "TagA", (" Hello, World! " + i).getBytes());
             SendResult sendResult = null;
             try {
-                sendResult = producer.send(message);
+                sendResult = producer.send(message, 1000);
                 System.out.println(sendResult);
 
             } catch (Exception e) {
